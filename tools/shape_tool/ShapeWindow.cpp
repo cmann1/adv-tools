701f6439e181fa4fa7d90c5143da0604eefcdb8a
Alex Morson <alexmorson@btinternet.com>
2023-11-04 14:04:15 +0000

    Add shape tool

---
+++ tools/shape_tool/ShapeWindow.cpp
@@ -0,0 +1,162 @@
+#include "../../../../lib/ui3/elements/TileShapeGraphic.cpp"
+
+const array<int> SHAPE_MAP = {
+	 8, 16,  0,  0,
+	 7, 15,  0,  0,
+	11,  3, 20, 17,
+	12,  4, 19, 18,
+	10,  9,  1,  2,
+	 6,  5, 13, 14
+};
+
+class ShapeWindow : Window
+{
+
+	private Container@ shape_container;
+	private Button@ custom_button;
+
+	private int _tile_shape = 0;
+	private int _selection_index = 2;
+
+	private bool _selecting_tiles = false;
+	private bool _using_custom_shape = false;
+
+	private TileShapes@ _selected_shape;
+
+	int tile_shape
+	{
+		get const { return _tile_shape; }
+		set
+		{
+			if(value < 0 or value > 20) return;
+			int index = SHAPE_MAP.find(value);
+			cast<Button>(shape_container.get_child(index)).selected = true;
+		}
+	}
+
+	bool selecting_tiles { get const { return _selecting_tiles; } }
+	bool using_custom_shape { get const { return _using_custom_shape; } }
+
+	ShapeWindow(UI@ ui)
+	{
+		super(ui, "Shape", false);
+
+		@layout = FlowLayout(ui, FlowDirection::Row);
+
+		@shape_container = Container(ui);
+
+		GridLayout@ grid = GridLayout(ui, 4);
+		grid.row_spacing = 0;
+		grid.column_spacing = 0;
+		@shape_container.layout = grid;
+
+		ButtonGroup@ button_group = ButtonGroup(ui, false);
+		button_group.select.on(EventCallback(on_button_select));
+
+		for(int i=0; i<24; ++i)
+		{
+			int tile_shape = SHAPE_MAP[i];
+			TileShapeGraphic@ t = TileShapeGraphic(ui, tile_shape);
+
+			Button@ b = Button(ui, t);
+			b.name = "" + i;
+			b.width = 48;
+			b.height = 48;
+			b.selectable = true;
+
+			if(i == 2) 
+				b.selected = true;
+
+			button_group.add(b);
+			shape_container.add_child(b);
+		}
+
+		shape_container.fit_to_contents();
+
+		Container@ custom_shape_container = Container(ui);
+		@custom_shape_container.layout = FlowLayout(ui, FlowDirection::Row);
+
+		Label@ custom_label = Label(ui, "Clone");
+		custom_label.width = 2 * 48 - 2 * ui.style.spacing;
+		custom_label.height = 48;
+		custom_label.align_h = GraphicAlign::Centre;
+		custom_label.align_v = GraphicAlign::Middle;
+
+		Button@ select_button = Button(ui, "X");
+		select_button.name = "select";
+		select_button.width = 48;
+		select_button.height = 48;
+		select_button.selectable = true;
+		@select_button.tooltip = PopupOptions(ui, "Select a region", false, PopupPosition::Below);
+
+		@custom_button = Button(ui, "O");
+		custom_button.name = "custom";
+		custom_button.width = 48;
+		custom_button.height = 48;
+		custom_button.selectable = true;
+		custom_button.disabled = true;
+		@custom_button.tooltip = PopupOptions(ui, "Paste tiles", false, PopupPosition::Below);
+
+		button_group.add(select_button);
+		button_group.add(custom_button);
+
+		custom_shape_container.add_child(custom_label);
+		custom_shape_container.add_child(select_button);
+		custom_shape_container.add_child(custom_button);
+
+		custom_shape_container.width = shape_container.width;
+		custom_shape_container.fit_to_contents(false);
+
+		add_child(shape_container);
+		add_child(custom_shape_container);
+
+		fit_to_contents(true);
+	}
+
+	void move_selection(int dx, int dy)
+	{
+		if(_selecting_tiles or _using_custom_shape) return;
+
+		const int x = (_selection_index % 4) + dx;
+		const int y = (_selection_index / 4) + dy;
+		if(x < 0 or x > 3 or y < 0 or y > 5) return;
+
+		const int index = 4 * y + x;
+		cast<Button>(shape_container.get_child(index)).selected = true;
+	}
+
+	void finished_selection()
+	{
+		custom_button.disabled = false;
+		custom_button.selected = true;
+	}
+	
+	// ///////////////////////////////////////////
+	// Events
+	// ///////////////////////////////////////////
+	
+	private void on_button_select(EventInfo@ event)
+	{
+		if(event.target is null) return;
+
+		const string name = event.target.name;
+		if(name == "select")
+		{
+			_selecting_tiles = true;
+			_using_custom_shape = false;
+		}
+		else if(name == "custom")
+		{
+			_selecting_tiles = false;
+			_using_custom_shape = true;
+		}
+		else
+		{
+			_selecting_tiles = false;
+			_using_custom_shape = false;
+			_selection_index = parseInt(event.target.name);
+			_tile_shape = SHAPE_MAP[_selection_index];
+		}
+	}
+
+}
---
+++ tools/shape_tool/TileEmbeds.cpp
@@ -0,0 +1,137 @@
+const string TILES_SPRITES_BASE = SPRITES_BASE + "tiles/";
+
+const string EMBED_spr_tile_1_1_1  = TILES_SPRITES_BASE + "tile_1_1_1.png";
+const string EMBED_spr_tile_1_1_2  = TILES_SPRITES_BASE + "tile_1_1_2.png";
+const string EMBED_spr_tile_1_1_3  = TILES_SPRITES_BASE + "tile_1_1_3.png";
+const string EMBED_spr_tile_1_2_1  = TILES_SPRITES_BASE + "tile_1_2_1.png";
+const string EMBED_spr_tile_1_2_2  = TILES_SPRITES_BASE + "tile_1_2_2.png";
+const string EMBED_spr_tile_1_2_3  = TILES_SPRITES_BASE + "tile_1_2_3.png";
+const string EMBED_spr_tile_1_3_1  = TILES_SPRITES_BASE + "tile_1_3_1.png";
+const string EMBED_spr_tile_1_3_2  = TILES_SPRITES_BASE + "tile_1_3_2.png";
+const string EMBED_spr_tile_1_3_3  = TILES_SPRITES_BASE + "tile_1_3_3.png";
+const string EMBED_spr_tile_1_4_1  = TILES_SPRITES_BASE + "tile_1_4_1.png";
+const string EMBED_spr_tile_1_4_2  = TILES_SPRITES_BASE + "tile_1_4_2.png";
+const string EMBED_spr_tile_1_4_3  = TILES_SPRITES_BASE + "tile_1_4_3.png";
+const string EMBED_spr_tile_1_5_1  = TILES_SPRITES_BASE + "tile_1_5_1.png";
+const string EMBED_spr_tile_1_5_2  = TILES_SPRITES_BASE + "tile_1_5_2.png";
+const string EMBED_spr_tile_1_5_3  = TILES_SPRITES_BASE + "tile_1_5_3.png";
+const string EMBED_spr_tile_1_6_1  = TILES_SPRITES_BASE + "tile_1_6_1.png";
+const string EMBED_spr_tile_1_6_2  = TILES_SPRITES_BASE + "tile_1_6_2.png";
+const string EMBED_spr_tile_1_6_3  = TILES_SPRITES_BASE + "tile_1_6_3.png";
+const string EMBED_spr_tile_1_7_1  = TILES_SPRITES_BASE + "tile_1_7_1.png";
+const string EMBED_spr_tile_1_7_2  = TILES_SPRITES_BASE + "tile_1_7_2.png";
+const string EMBED_spr_tile_1_7_3  = TILES_SPRITES_BASE + "tile_1_7_3.png";
+const string EMBED_spr_tile_1_8_1  = TILES_SPRITES_BASE + "tile_1_8_1.png";
+const string EMBED_spr_tile_1_9_1  = TILES_SPRITES_BASE + "tile_1_9_1.png";
+const string EMBED_spr_tile_1_10_1 = TILES_SPRITES_BASE + "tile_1_10_1.png";
+const string EMBED_spr_tile_1_11_1 = TILES_SPRITES_BASE + "tile_1_11_1.png";
+const string EMBED_spr_tile_1_12_1 = TILES_SPRITES_BASE + "tile_1_12_1.png";
+const string EMBED_spr_tile_1_13_1 = TILES_SPRITES_BASE + "tile_1_13_1.png";
+const string EMBED_spr_tile_1_14_1 = TILES_SPRITES_BASE + "tile_1_14_1.png";
+const string EMBED_spr_tile_1_14_2 = TILES_SPRITES_BASE + "tile_1_14_2.png";
+const string EMBED_spr_tile_1_14_3 = TILES_SPRITES_BASE + "tile_1_14_3.png";
+const string EMBED_spr_tile_1_15_1 = TILES_SPRITES_BASE + "tile_1_15_1.png";
+const string EMBED_spr_tile_1_15_2 = TILES_SPRITES_BASE + "tile_1_15_2.png";
+const string EMBED_spr_tile_1_15_3 = TILES_SPRITES_BASE + "tile_1_15_3.png";
+const string EMBED_spr_tile_1_16_1 = TILES_SPRITES_BASE + "tile_1_16_1.png";
+const string EMBED_spr_tile_1_16_2 = TILES_SPRITES_BASE + "tile_1_16_2.png";
+const string EMBED_spr_tile_1_16_3 = TILES_SPRITES_BASE + "tile_1_16_3.png";
+const string EMBED_spr_tile_1_17_1 = TILES_SPRITES_BASE + "tile_1_17_1.png";
+const string EMBED_spr_tile_1_18_1 = TILES_SPRITES_BASE + "tile_1_18_1.png";
+const string EMBED_spr_tile_1_19_1 = TILES_SPRITES_BASE + "tile_1_19_1.png";
+const string EMBED_spr_tile_1_20_1 = TILES_SPRITES_BASE + "tile_1_20_1.png";
+const string EMBED_spr_tile_1_21_1 = TILES_SPRITES_BASE + "tile_1_21_1.png";
+
+const string EMBED_spr_tile_2_1_1  = TILES_SPRITES_BASE + "tile_2_1_1.png";
+const string EMBED_spr_tile_2_1_2  = TILES_SPRITES_BASE + "tile_2_1_2.png";
+const string EMBED_spr_tile_2_1_3  = TILES_SPRITES_BASE + "tile_2_1_3.png";
+const string EMBED_spr_tile_2_2_1  = TILES_SPRITES_BASE + "tile_2_2_1.png";
+const string EMBED_spr_tile_2_2_2  = TILES_SPRITES_BASE + "tile_2_2_2.png";
+const string EMBED_spr_tile_2_2_3  = TILES_SPRITES_BASE + "tile_2_2_3.png";
+const string EMBED_spr_tile_2_3_1  = TILES_SPRITES_BASE + "tile_2_3_1.png";
+const string EMBED_spr_tile_2_3_2  = TILES_SPRITES_BASE + "tile_2_3_2.png";
+const string EMBED_spr_tile_2_3_3  = TILES_SPRITES_BASE + "tile_2_3_3.png";
+const string EMBED_spr_tile_2_4_1  = TILES_SPRITES_BASE + "tile_2_4_1.png";
+const string EMBED_spr_tile_2_4_2  = TILES_SPRITES_BASE + "tile_2_4_2.png";
+const string EMBED_spr_tile_2_5_1  = TILES_SPRITES_BASE + "tile_2_5_1.png";
+const string EMBED_spr_tile_2_5_2  = TILES_SPRITES_BASE + "tile_2_5_2.png";
+const string EMBED_spr_tile_2_6_1  = TILES_SPRITES_BASE + "tile_2_6_1.png";
+const string EMBED_spr_tile_2_6_2  = TILES_SPRITES_BASE + "tile_2_6_2.png";
+const string EMBED_spr_tile_2_7_1  = TILES_SPRITES_BASE + "tile_2_7_1.png";
+const string EMBED_spr_tile_2_7_2  = TILES_SPRITES_BASE + "tile_2_7_2.png";
+const string EMBED_spr_tile_2_8_1  = TILES_SPRITES_BASE + "tile_2_8_1.png";
+const string EMBED_spr_tile_2_8_2  = TILES_SPRITES_BASE + "tile_2_8_2.png";
+const string EMBED_spr_tile_2_9_1  = TILES_SPRITES_BASE + "tile_2_9_1.png";
+const string EMBED_spr_tile_2_9_2  = TILES_SPRITES_BASE + "tile_2_9_2.png";
+const string EMBED_spr_tile_2_9_3  = TILES_SPRITES_BASE + "tile_2_9_3.png";
+const string EMBED_spr_tile_2_10_1 = TILES_SPRITES_BASE + "tile_2_10_1.png";
+const string EMBED_spr_tile_2_11_1 = TILES_SPRITES_BASE + "tile_2_11_1.png";
+const string EMBED_spr_tile_2_11_2 = TILES_SPRITES_BASE + "tile_2_11_2.png";
+const string EMBED_spr_tile_2_11_3 = TILES_SPRITES_BASE + "tile_2_11_3.png";
+const string EMBED_spr_tile_2_12_1 = TILES_SPRITES_BASE + "tile_2_12_1.png";
+const string EMBED_spr_tile_2_12_2 = TILES_SPRITES_BASE + "tile_2_12_2.png";
+const string EMBED_spr_tile_2_12_3 = TILES_SPRITES_BASE + "tile_2_12_3.png";
+const string EMBED_spr_tile_2_13_1 = TILES_SPRITES_BASE + "tile_2_13_1.png";
+
+const string EMBED_spr_tile_3_1_1  = TILES_SPRITES_BASE + "tile_3_1_1.png";
+const string EMBED_spr_tile_3_1_2  = TILES_SPRITES_BASE + "tile_3_1_2.png";
+const string EMBED_spr_tile_3_1_3  = TILES_SPRITES_BASE + "tile_3_1_3.png";
+const string EMBED_spr_tile_3_1_4  = TILES_SPRITES_BASE + "tile_3_1_4.png";
+const string EMBED_spr_tile_3_2_1  = TILES_SPRITES_BASE + "tile_3_2_1.png";
+const string EMBED_spr_tile_3_2_2  = TILES_SPRITES_BASE + "tile_3_2_2.png";
+const string EMBED_spr_tile_3_3_1  = TILES_SPRITES_BASE + "tile_3_3_1.png";
+const string EMBED_spr_tile_3_3_2  = TILES_SPRITES_BASE + "tile_3_3_2.png";
+const string EMBED_spr_tile_3_4_1  = TILES_SPRITES_BASE + "tile_3_4_1.png";
+const string EMBED_spr_tile_3_5_1  = TILES_SPRITES_BASE + "tile_3_5_1.png";
+const string EMBED_spr_tile_3_6_1  = TILES_SPRITES_BASE + "tile_3_6_1.png";
+
+const string EMBED_spr_tile_4_1_1  = TILES_SPRITES_BASE + "tile_4_1_1.png";
+const string EMBED_spr_tile_4_1_2  = TILES_SPRITES_BASE + "tile_4_1_2.png";
+const string EMBED_spr_tile_4_1_3  = TILES_SPRITES_BASE + "tile_4_1_3.png";
+const string EMBED_spr_tile_4_1_4  = TILES_SPRITES_BASE + "tile_4_1_4.png";
+const string EMBED_spr_tile_4_2_1  = TILES_SPRITES_BASE + "tile_4_2_1.png";
+const string EMBED_spr_tile_4_2_2  = TILES_SPRITES_BASE + "tile_4_2_2.png";
+const string EMBED_spr_tile_4_3_1  = TILES_SPRITES_BASE + "tile_4_3_1.png";
+const string EMBED_spr_tile_4_4_1  = TILES_SPRITES_BASE + "tile_4_4_1.png";
+const string EMBED_spr_tile_4_4_2  = TILES_SPRITES_BASE + "tile_4_4_2.png";
+const string EMBED_spr_tile_4_4_3  = TILES_SPRITES_BASE + "tile_4_4_3.png";
+const string EMBED_spr_tile_4_4_4  = TILES_SPRITES_BASE + "tile_4_4_4.png";
+const string EMBED_spr_tile_4_5_1  = TILES_SPRITES_BASE + "tile_4_5_1.png";
+const string EMBED_spr_tile_4_5_2  = TILES_SPRITES_BASE + "tile_4_5_2.png";
+const string EMBED_spr_tile_4_5_3  = TILES_SPRITES_BASE + "tile_4_5_3.png";
+const string EMBED_spr_tile_4_5_4  = TILES_SPRITES_BASE + "tile_4_5_4.png";
+const string EMBED_spr_tile_4_6_1  = TILES_SPRITES_BASE + "tile_4_6_1.png";
+const string EMBED_spr_tile_4_6_2  = TILES_SPRITES_BASE + "tile_4_6_2.png";
+const string EMBED_spr_tile_4_6_3  = TILES_SPRITES_BASE + "tile_4_6_3.png";
+const string EMBED_spr_tile_4_6_4  = TILES_SPRITES_BASE + "tile_4_6_4.png";
+const string EMBED_spr_tile_4_7_1  = TILES_SPRITES_BASE + "tile_4_7_1.png";
+const string EMBED_spr_tile_4_7_2  = TILES_SPRITES_BASE + "tile_4_7_2.png";
+const string EMBED_spr_tile_4_7_3  = TILES_SPRITES_BASE + "tile_4_7_3.png";
+const string EMBED_spr_tile_4_7_4  = TILES_SPRITES_BASE + "tile_4_7_4.png";
+const string EMBED_spr_tile_4_8_1  = TILES_SPRITES_BASE + "tile_4_8_1.png";
+const string EMBED_spr_tile_4_8_2  = TILES_SPRITES_BASE + "tile_4_8_2.png";
+const string EMBED_spr_tile_4_8_3  = TILES_SPRITES_BASE + "tile_4_8_3.png";
+const string EMBED_spr_tile_4_8_4  = TILES_SPRITES_BASE + "tile_4_8_4.png";
+const string EMBED_spr_tile_4_9_1  = TILES_SPRITES_BASE + "tile_4_9_1.png";
+
+const string EMBED_spr_tile_5_1_1  = TILES_SPRITES_BASE + "tile_5_1_1.png";
+const string EMBED_spr_tile_5_1_2  = TILES_SPRITES_BASE + "tile_5_1_2.png";
+const string EMBED_spr_tile_5_1_3  = TILES_SPRITES_BASE + "tile_5_1_3.png";
+const string EMBED_spr_tile_5_1_4  = TILES_SPRITES_BASE + "tile_5_1_4.png";
+const string EMBED_spr_tile_5_1_5  = TILES_SPRITES_BASE + "tile_5_1_5.png";
+const string EMBED_spr_tile_5_1_6  = TILES_SPRITES_BASE + "tile_5_1_6.png";
+const string EMBED_spr_tile_5_2_1  = TILES_SPRITES_BASE + "tile_5_2_1.png";
+
+void build_tile_sprites(message@ msg)
+{
+	for(uint sprite_set=1; sprite_set<=TILE_COUNTS.size(); ++sprite_set)
+	{
+		for(uint sprite_tile=1; sprite_tile<=TILE_COUNTS[sprite_set-1].size(); ++sprite_tile)
+		{
+			for(int sprite_palette=1; sprite_palette<=TILE_COUNTS[sprite_set-1][sprite_tile-1]; ++sprite_palette)
+			{
+				build_sprite(msg, "tile_" + sprite_set + "_" + sprite_tile + "_" + sprite_palette);
+			}
+		}
+	}
+}
